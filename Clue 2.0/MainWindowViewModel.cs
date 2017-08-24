0942851095a75a2ff315be6e71a92dc16d1798e5
Karina Sisnero <ksisnero@practicevelocity.com>
2017-08-24 16:28:29 -0500

    initial commit

---
+++ Clue 2.0/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Clue_2._0.ViewModels;
+using DevExpress.Mvvm;
+
+namespace Clue_2._0
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public virtual IntroWindowViewModel IntroWindow { get; set; }
+        public virtual RoomWindowViewModel RoomWindow { get; set; }
+        public virtual WeaponWindowViewModel WeaponWindow { get; set; }
+        public virtual SuspectWindowViewModel SuspectWindow { get; set; }
+        public virtual GameOverWindowViewModel GameOverWindow { get; set; }
+        public virtual WindowOrganizer WindowSet { get; set; }
+
+        public MainWindowViewModel()
+        {
+            IntroWindow = new IntroWindowViewModel();
+
+            SuspectWindow = new SuspectWindowViewModel();
+
+            WindowSet = WindowOrganizer.Create(IntroWindow);
+        }
+    }
+}
---
+++ Clue 2.0/Views/IntroWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Clue_2._0.Views
+{
+    /// <summary>
+    /// Interaction logic for IntroWindow.xaml
+    /// </summary>
+    public partial class IntroWindow : UserControl
+    {
+        public IntroWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
